a0dbcf5861682e9ff15dc530a78c08bb38d5717c
Jordi Adan <jordiadan@gmail.com>
2022-03-08 00:00:06 +0100

    Remove repeated code

--- src/main/kotlin/com/example/fitbitsubscriber/application/GetWeightFromFitbitUseCase.kt
+++ src/main/kotlin/com/example/fitbitsubscriber/application/GetWeightFromFitbitUseCase.kt
@@ -8,38 +8,4 @@ class GetWeightFromFitbitUseCase(private val fitbitRepository: FitbitRepository)
         val weightLog = fitbitRepository.getWeightLog(query.userId, query.date)
         return weightLog.weight
     }
-
-    fun printNonNull(str: String?) {
-        println("Printing \"$str\":")
-
-        str?.let {                         // 4
-            print("\t")
-            customPrint(it)
-            println()
-        }
-    }
-
-    private fun customPrint(it: String) {
-        TODO("Not yet implemented")
-    }
-
-    fun printNonNul(str: String?) {
-        println("Printing \"$str\":")
-
-        str?.let {                         // 4
-            print("\t")
-            customPrint(it)
-            println()
-        }
-    }
-
-    fun printNonNl(str: String?) {
-        println("Printing \"$str\":")
-
-        str?.let {                         // 4
-            print("\t")
-            customPrint(it)
-            println()
-        }
-    }
 }

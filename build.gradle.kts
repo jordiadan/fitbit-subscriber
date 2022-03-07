3d4ec8574d827bd09105d1056d1e53db0f1e4e57
Jordi Adan <jordiadan@gmail.com>
2022-03-07 20:09:48 +0100

    Add sonarqube

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
-    jacoco
-    id("com.github.nbaztec.coveralls-jacoco") version "1.2.14"
+    id("org.sonarqube") version "3.3"
 }
 
 group = "com.example"
@@ -35,9 +34,10 @@ tasks.withType<Test> {
     useJUnitPlatform()
 }
 
-buildscript {
-    repositories {
-        mavenCentral()
-        jcenter()
+sonarqube {
+    properties {
+        property("sonar.projectKey", "jordiadan_fitbit-subscriber")
+        property("sonar.organization", "jordiadan")
+        property("sonar.host.url", "https://sonarcloud.io")
     }
 }
